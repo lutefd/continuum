33f4a9be9f21f96c285c3c7d8999eb1f8922ccfd
Luis Dourado <lfaddourado@gmail.com>
2026-05-24 18:35:02 -0300

    Initial Commit

---
+++ continuum/continuumApp.swift
@@ -0,0 +1,32 @@
+//
+//  continuumApp.swift
+//  continuum
+//
+//  Created by Luis Dourado on 24/05/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct continuumApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
